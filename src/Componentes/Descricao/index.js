6edfe9a20abbf5bbb554fdef9dc65f3277dca785
Guilherme <guih_so@hotmail.com>
2022-09-08 19:08:30 -0300

    Responsivo das secoes Header e Descriao feitos

--- src/Componentes/Descricao/index.js
+++ src/Componentes/Descricao/index.js
@@ -3,13 +3,13 @@ import './Descricao.css'
 const Descricao = () => {
     return (
         <header className="descricao">
-            <img src="./Imagens/Banner.png" alt="" />
+            <img className='banner1' src="./Imagens/Banner.png" alt="" />
             <div>
-             <h1>Nós buscamos por excelência em tudo!</h1>
-             <p>Somos profissionais treinados e com experiência, buscamos sempre por qualidade e garantia em todos os nossos serviços.</p>
-             <button>NOSSOS SERVICOS</button>
+                <h1>Nós buscamos por excelência em tudo!</h1>
+                <p>Somos profissionais treinados e com experiência, buscamos sempre por qualidade e garantia em todos os nossos serviços.</p>
+                <button>NOSSOS SERVICOS</button>
             </div>
-            <img src='./imagens/Banner-2.png' alt='' />
+            <img className='banner2' src='./imagens/Banner-2.png' alt='' />
         </header>
     )
 }
